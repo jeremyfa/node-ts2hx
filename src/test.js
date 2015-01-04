218ff20cf0f5213a275677cd211baf9bffce5450
jeremyfa <jeremy.faivre@gmail.com>
2015-01-04 13:31:34 +0100

    Fix test.js path

--- src/test.js
+++ src/test.js
@@ -2,6 +2,6 @@
 var fs = require('fs');
 var ts2hx = require('./ts2hx');
 
-var hx = ts2hx(fs.readFileSync(__dirname+'/../Example.ts'));
+var hx = ts2hx(fs.readFileSync(__dirname+'/../example/Example.ts'));
 
 process.stdout.write(hx);
